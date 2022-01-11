99d563f9afa4abed46f56de5251d9ae916dc0dd5
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-12 02:42:11 +0700

    Inisialisasi Project Java Logging

---
+++ src/main/java/com/hanifiamdev/logging/BelajarJavaLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaLoggingApplication.class, args);
+	}
+
+}
